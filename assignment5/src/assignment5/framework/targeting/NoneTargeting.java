fb410a50912f9a31ce19a919578f431e142c4389
Sylvain Kuchen <sylvaink@bluewin.ch>
2019-12-09 11:30:25 +0100

    implement NoneTargeting

---
+++ assignment5/src/assignment5/framework/targeting/NoneTargeting.java
@@ -0,0 +1,9 @@
+package assignment5.framework.targeting;
+
+import robocode.AdvancedRobot;
+
+public class NoneTargeting extends Targeting {
+	public NoneTargeting(AdvancedRobot robot) {
+		super(robot);
+	}
+}
