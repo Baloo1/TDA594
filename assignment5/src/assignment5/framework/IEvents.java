dd9cbb17f809e50a3fcd65b1dfd742716479fab6
Johannes Mattsson <karlmattsson96@gmail.com>
2019-12-09 12:49:16 +0100

    Remove scoping for interface methods

--- assignment5/src/assignment5/framework/IEvents.java
+++ assignment5/src/assignment5/framework/IEvents.java
@@ -3,8 +3,8 @@ package assignment5.framework;
 import robocode.*;
 
 public interface IEvents {
-	public void onScannedRobot(ScannedRobotEvent e);
-	public void onHitByBullet(HitByBulletEvent e);
-	public void onDeath(DeathEvent e);
-	public void onBulletHit(BulletHitEvent e);
+	void onScannedRobot(ScannedRobotEvent e);
+	void onHitByBullet(HitByBulletEvent e);
+	void onDeath(DeathEvent e);
+	void onBulletHit(BulletHitEvent e);
 }
