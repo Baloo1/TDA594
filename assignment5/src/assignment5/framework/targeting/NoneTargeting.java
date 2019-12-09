9b5b89b0ca0c45120c2e503a7e89eb5565790d45
Johannes Mattsson <karlmattsson96@gmail.com>
2019-12-09 16:20:45 +0100

    Add comments for NoneTargeting

--- assignment5/src/assignment5/framework/targeting/NoneTargeting.java
+++ assignment5/src/assignment5/framework/targeting/NoneTargeting.java
@@ -4,13 +4,19 @@ import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 
 public class NoneTargeting extends AbstractTargeting {
+	/**
+	 * @param robot an instance of a RoboCode robot
+	 */
 	public NoneTargeting(AdvancedRobot robot) {
 		super(robot);
 	}
 
+	/**
+	 * Not implemented, not needed
+	 */
 	@Override
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
