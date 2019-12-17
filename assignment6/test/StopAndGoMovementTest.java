e702f17ef7ac7c5aa2e5cba702a43c52df85b444
Kajsa <bjarang@student.chalmers.se>
2019-12-17 13:12:22 +0100

    add test onBulletHit and setDeltaTest

--- assignment6/test/StopAndGoMovementTest.java
+++ assignment6/test/StopAndGoMovementTest.java
@@ -2,9 +2,8 @@ import assignment6.framework.movement.StopAndGoMovement;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import robocode.AdvancedRobot;
-import robocode.Bullet;
-import robocode.HitByBulletEvent;
+
+import robocode.*;
 
 public class StopAndGoMovementTest {
 
@@ -12,11 +11,11 @@ public class StopAndGoMovementTest {
     public void onScannedRobotTest() {
         AdvancedRobot robot = new AdvancedRobot();
         StopAndGoMovement tester = new StopAndGoMovement(robot);
-        assert
     }
 
     @Test
     public void onHitByBulletTest() {
+       // HitByBulletEvent event = new HitByBulletEvent();
         HitByBulletEvent event = new HitByBulletEvent(2/Math.PI, new Bullet(2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1));
 
         AdvancedRobot robot = new AdvancedRobot();
@@ -25,5 +24,46 @@ public class StopAndGoMovementTest {
         tester.onHitByBullet(event);
         assertEquals(tester.lastEnemyEnergy, 9, 0);
     }
+
+    @Test
+    public void onBulletHitTest() {
+        BulletHitEvent event = new BulletHitEvent("robot", 10, new Bullet (2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1) );
+
+        AdvancedRobot robot = new AdvancedRobot();
+        StopAndGoMovement tester = new StopAndGoMovement(robot);
+
+        tester.onBulletHit(event);
+        System.out.println(tester.lastEnemyEnergy);
+        assertEquals(tester.lastEnemyEnergy, -10, 0);
+    }
+
+    @Test
+    public void setDeltaTest(){
+        ScannedRobotEvent event = new ScannedRobotEvent("robot", 10, 0,0,0,0,true);
+
+        AdvancedRobot robot = new AdvancedRobot();
+        StopAndGoMovement tester = new StopAndGoMovement(robot);
+
+        tester.setDelta(event);
+        System.out.println(event.getEnergy());
+        assertEquals(tester.setDelta(event), -10, 0);
+    }
+
+
+    /*
+    @Test
+    public void onDeathTest() {
+        DeathEvent event = new DeathEvent();
+
+        AdvancedRobot robot = new AdvancedRobot();
+        System.out.println(robot.getRoundNum());
+        StopAndGoMovement tester = new StopAndGoMovement(robot);
+
+        tester.onDeath(event);
+
+        assertTrue(tester.flat);
+
+    }
+     */
 
 }
