9daf64b336241ac9ff277269c0dfc3aa06325e62
Kajsa <bjarang@student.chalmers.se>
2019-12-18 20:39:40 +0100

    add tests on linearTargeting

--- assignment6/src/assignment6/framework/targeting/LinearTargeting.java
+++ assignment6/src/assignment6/framework/targeting/LinearTargeting.java
@@ -17,23 +17,23 @@ public class LinearTargeting extends AbstractTargeting {
 	 */
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		final double bulletPower = Math.min(3.0, robot.getEnergy());
-		final double myX = robot.getX();
-		final double myY = robot.getY();
-		final double absoluteBearing = robot.getHeadingRadians() + event.getBearingRadians();
-		final double enemyX = robot.getX() + event.getDistance() * Math.sin(absoluteBearing);
-		final double enemyY = robot.getY() + event.getDistance() * Math.cos(absoluteBearing);
-		final double enemyHeading = event.getHeadingRadians();
-		final double enemyVelocity = event.getVelocity();
-		double deltaTime = 1;
+		//DONE
+		final double bulletPower = getBulletPower(robot.getEnergy());
+		final double absoluteBearing = getAbsoluteBearing(robot, event);
+
+		double enemyX = getEnemyX(robot, event, absoluteBearing);
+		double enemyY = getEnemyY(robot, event, absoluteBearing);
 
 		final double battleFieldHeight = robot.getBattleFieldHeight();
 		final double battleFieldWidth = robot.getBattleFieldWidth();
+
 		double predictedX = enemyX;
 		double predictedY = enemyY;
-		while (deltaTime * (20.0 - 3.0 * bulletPower) < Point2D.Double.distance(myX, myY, predictedX, predictedY)) {
-			predictedX += Math.sin(enemyHeading) * enemyVelocity;
-			predictedY += Math.cos(enemyHeading) * enemyVelocity;
+
+		double deltaTime = 1;
+		while (deltaTime * (20.0 - 3.0 * bulletPower) < Point2D.Double.distance(robot.getX(), robot.getY(), predictedX, predictedY)) {
+			predictedX += Math.sin(event.getHeadingRadians()) * event.getVelocity();
+			predictedY += Math.cos(event.getHeadingRadians()) * event.getVelocity();
 			if (predictedX < 18.0 || predictedY < 18.0 || predictedX > battleFieldWidth - 18.0
 					|| predictedY > battleFieldHeight - 18.0) {
 				predictedX = Math.min(Math.max(18.0, predictedX), battleFieldWidth - 18.0);
@@ -42,11 +42,34 @@ public class LinearTargeting extends AbstractTargeting {
 			}
 			deltaTime++;
 		}
-		final double theta = Utils
-				.normalAbsoluteAngle(Math.atan2(predictedX - robot.getX(), predictedY - robot.getY()));
+
+		//DONE
+		final double theta = getTheta(predictedX, predictedY);
+
 		robot.setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - robot.getRadarHeadingRadians()));
 		robot.setTurnGunRightRadians(Utils.normalRelativeAngle(theta - robot.getGunHeadingRadians()));
 		robot.fire(bulletPower);
 	}
+
+	public double getEnemyY(AdvancedRobot robot, ScannedRobotEvent event, double absoluteBearing) {
+		return robot.getY() + event.getDistance() * Math.cos(absoluteBearing);
+	}
+
+	public double getEnemyX(AdvancedRobot robot, ScannedRobotEvent event, double absoluteBearing) {
+		return robot.getX() + event.getDistance() * Math.sin(absoluteBearing);
+	}
+
+	public double getAbsoluteBearing(AdvancedRobot robot, ScannedRobotEvent event) {
+		return robot.getHeadingRadians() + event.getBearingRadians();
+	}
+
+	public double getBulletPower(double energy) {
+		return Math.min(3.0, energy);
+	}
+
+	public double getTheta(double predictedX, double predictedY) {
+		return Utils.normalAbsoluteAngle(Math.atan2(predictedX - robot.getX(), predictedY - robot.getY()));
+	}
+
 
 }
