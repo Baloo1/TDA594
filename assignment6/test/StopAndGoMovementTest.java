1ed1ae442b8527ef3c732daf08a17d1a02268fca
Johannes Mattsson <karlmattsson96@gmail.com>
2019-12-18 16:15:59 +0100

    Add before part to StopAndGoTest

--- assignment6/test/StopAndGoMovementTest.java
+++ assignment6/test/StopAndGoMovementTest.java
@@ -1,6 +1,7 @@
 import Mock.MockAdvancedRobot;
 import assignment6.framework.movement.StopAndGoMovement;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import robocode.*;
@@ -9,49 +10,56 @@ import static org.junit.Assert.*;
 
 
 public class StopAndGoMovementTest {
-    private MockAdvancedRobot robot = new MockAdvancedRobot();
-    private StopAndGoMovement tester = new StopAndGoMovement(robot);
+    private MockAdvancedRobot MockRobot;
+    private StopAndGoMovement StopAndGoTester;
 
-    //TODO: create test for onScannedRobotTest
+    @Before
+    public void before() {
+        MockRobot = new MockAdvancedRobot();
+        StopAndGoTester = new StopAndGoMovement(MockRobot);
+    }
+
     @Test
     public void onScannedRobotTest() {
+        ScannedRobotEvent event = new ScannedRobotEvent();
+
+//        StopAndGoTester.onScannedRobot(event);
     }
 
     @Test
     public void onHitByBulletTest() {
         HitByBulletEvent event = new HitByBulletEvent(2/Math.PI, new Bullet(2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1));
 
-        tester.onHitByBullet(event);
-        assertEquals(9, tester.getLastEnemyEnergy(), 0);
+        StopAndGoTester.onHitByBullet(event);
+        assertEquals(9, StopAndGoTester.getLastEnemyEnergy(), 0);
     }
 
     @Test
     public void onDeathLowRoundTest(){
         DeathEvent event = new DeathEvent();
 
-        tester.onDeath(event);
-        assertTrue(tester.isFlat());
+        StopAndGoTester.onDeath(event);
+        assertTrue(StopAndGoTester.isFlat());
     }
 
     @Test
     public void onDeathHighRoundTest(){
         DeathEvent event = new DeathEvent();
-        robot.setRoundNum(4);
-        tester.onDeath(event);
-        assertFalse(tester.isFlat());
+        MockRobot.setRoundNum(4);
+        StopAndGoTester.onDeath(event);
+        assertFalse(StopAndGoTester.isFlat());
     }
 
     @Test
     public void onBulletHitTest() {
         BulletHitEvent event = new BulletHitEvent("robot", 10, new Bullet (2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1) );
-        tester.onBulletHit(event);
-        assertEquals(-1, tester.getLastEnemyEnergy(), 0);
+        StopAndGoTester.onBulletHit(event);
+        assertEquals(-1, StopAndGoTester.getLastEnemyEnergy(), 0);
     }
 
     @Test
     public void setDeltaTest(){
         ScannedRobotEvent event = new ScannedRobotEvent("robot", 10, 0,0,0,0,true);
-        assertEquals(-11, tester.setDelta(event), 0);
+        assertEquals(-11, StopAndGoTester.setDelta(event), 0);
     }
-
 }
