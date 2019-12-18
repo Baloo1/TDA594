03fca4a40092a9f9b50d70dfc5cf912ecc8b830a
Johannes Mattsson <karlmattsson96@gmail.com>
2019-12-18 16:11:55 +0100

    Add more methods to MockAdvancedRobot

--- assignment6/test/Mock/MockAdvancedRobot.java
+++ assignment6/test/Mock/MockAdvancedRobot.java
@@ -4,6 +4,11 @@ import robocode.AdvancedRobot;
 
 public class MockAdvancedRobot extends AdvancedRobot {
   private int roundNum = 0;
+  private double headingRadians = 0;
+  private double x = 0;
+  private double y = 0;
+  private double turnRightRadians = 0;
+  private double ahead = 0;
 
   @Override
   public int getRoundNum() {
@@ -14,4 +19,40 @@ public class MockAdvancedRobot extends AdvancedRobot {
     this.roundNum=round;
   }
 
+  @Override
+  public double getHeadingRadians() {
+    return headingRadians;
+  }
+
+  public void setHeadingRadians(double heading) {
+    this.headingRadians = heading;
+  }
+
+  @Override
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  @Override
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  @Override
+  public void setTurnRightRadians(double rightRadians) {
+    this.turnRightRadians = rightRadians;
+  }
+
+  @Override
+  public void setAhead(double ahead) {
+    this.ahead = ahead;
+  }
 }
