cf5d5a7cdd2bf6bcdd4b39415bab1cb357e5d6cf
Johannes Mattsson <karlmattsson96@gmail.com>
2019-12-18 14:12:29 +0100

    Add delta to Asserts

--- assignment6/test/StopAndGoMovementTest.java
+++ assignment6/test/StopAndGoMovementTest.java
@@ -19,7 +19,7 @@ public class StopAndGoMovementTest {
         HitByBulletEvent event = new HitByBulletEvent(2/Math.PI, new Bullet(2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1));
 
         tester.onHitByBullet(event);
-        assertEquals(tester.getLastEnemyEnergy(), 9, 0);
+        assertEquals(9, tester.getLastEnemyEnergy(), 0);
     }
 
     //TODO: Create test for method onDeath
@@ -31,13 +31,13 @@ public class StopAndGoMovementTest {
     public void onBulletHitTest() {
         BulletHitEvent event = new BulletHitEvent("robot", 10, new Bullet (2/Math.PI, 0,0, 1000, "Attacker", "Defender", true, 1) );
         tester.onBulletHit(event);
-        assertEquals(tester.getLastEnemyEnergy(), -10, 0);
+        assertEquals(-1, tester.getLastEnemyEnergy(), 0);
     }
 
     @Test
     public void setDeltaTest(){
         ScannedRobotEvent event = new ScannedRobotEvent("robot", 10, 0,0,0,0,true);
-        assertEquals(tester.setDelta(event), -10, 0);
+        assertEquals(-11, tester.setDelta(event), 0);
     }
 
 }
